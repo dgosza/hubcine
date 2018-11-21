95508909ff40b703829a073c723f0ad8ea2e7f37
Diego Souza <diegomagasouza@gmail.com>
2018-11-21 17:09:41 -0200

    deletar filme selecionado

---
+++ painel_adm_deletefilme.php
@@ -0,0 +1,23 @@
+<?php
+   session_start();
+   include_once 'conectaBanco.php';
+
+
+
+   $id = filter_input(INPUT_GET, 'idFilme', FILTER_SANITIZE_NUMBER_INT);
+
+    $envia = "UPDATE filmes set mostrar = 0 where idFilme = '$id'";
+
+    $insere_bd = $conecta->prepare($envia);
+
+    if($insere_bd->execute()){
+        header("Location: filmeExcluido.php");
+    }else{
+        echo 'failed';
+    }
+        
+   
+?>
+
+
+
